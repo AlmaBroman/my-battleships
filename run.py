7db53273f2b00ddca9362724428ac6f80a883f87
AlmaBroman <>
2023-10-31 11:05:17 +0000

    Change method for building game board, sketch next step

--- run.py
+++ run.py
@@ -6,26 +6,32 @@ def start_message():
     print('MY BATTLESHIPS\n')
     print('Try to sink all the ships, before')
     print('the computer sinks all of yours!\n')
-    print('Board size: 5 x 5')
+    print('Board size: 4 x 5')
     print('Top left is row 0, column 0.')
 
 
-def game_board():
-    """
-    Creates an empty gameboard
-    """
-    column = ''
-    row = [column] * 5
-    gameBoard = [row] * 5
-    print(gameBoard)
+# Define empty gameboard
+ROWS = 5
+COLUMNS = 4
+game_board = [[' .'] * COLUMNS for num in range(ROWS)]
+
+
+def show_game_board():
+    for i in game_board:
+        print(*i)
 
 
 def main():
     start_message()
     # get username
     username = input("Type your name and press return: ")
-    print(f"\nHi {username} let's play!")
+    print(f"\nHi {username} let's play!\n")
+    # show players board with ships!!
+    print("Your board:")
+    show_game_board()
+    # show computer´s current board!!
+    print("\nComputer´s board: ")
+    show_game_board()
 
 
-# game_board()
 main()
